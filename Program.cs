9d2ce018db3a219f0b41f58fef2d9d095bd49cca
HagarElbakry99 <121252814+HagarElbakry99@users.noreply.github.com>
2022-12-23 15:24:28 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perfect_Number_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter start number = ");
+            int start = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter End number = ");
+            int end = int.Parse(Console.ReadLine());
+            for (int i = start; i <= end; i++)
+            {
+                int sum = 0;
+                for (int j = 1; j < i; j++)
+                {
+                    if (i % j == 0)
+                        sum = sum + j;
+
+                }
+                if (sum == i)
+                {
+                    Console.WriteLine(i + " is perfect number ");
+                }
+
+            }
+        }
+    }
+}
